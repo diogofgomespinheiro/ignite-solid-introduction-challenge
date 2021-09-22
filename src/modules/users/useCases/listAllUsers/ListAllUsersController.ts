6655aa36e48cc0a5d2c6136f1610f15755cf24fc
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-09-22 22:44:30 +0100

    ✨ feat: finish controllers

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,14 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers as { user_id: string };
+
+    try {
+      const user = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 

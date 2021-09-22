0dea9d322ce05e08d226c3fbf6812c6b3e9429db
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-09-22 22:26:25 +0100

    ✨ feat: finish model

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,36 @@
 import { v4 as uuidV4 } from "uuid";
 
+export interface IUserProps {
+  id?: string;
+  name: string;
+  email: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
 class User {
-  // Complete aqui
+  id?: string;
+  name: string;
+  admin?: boolean;
+  email: string;
+  created_at?: Date;
+  updated_at?: Date;
+
+  private constructor(props: IUserProps) {
+    if (!this.id) {
+      this.id = props.id || uuidV4();
+    }
+
+    this.name = props.name;
+    this.admin = false;
+    this.email = props.email;
+    this.created_at = props.created_at || new Date();
+    this.updated_at = props.updated_at || new Date();
+  }
+
+  public static create(props: IUserProps): User {
+    return new User(props);
+  }
 }
 
 export { User };
